94f643d307593b6fca8ef800075628934aee5cf7
Dima Sukhariev <dmitriu.sy1303@gmail.com>
2026-05-10 15:56:48 +0300

    fix import formatting and translate error messages to English

--- app/cafe.py
+++ app/cafe.py
@@ -12,13 +12,13 @@ class Cafe:
 
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
-            raise NotVaccinatedError("Немає вакцини")
+            raise NotVaccinatedError("There is no vaccine")
 
         vaccine_expiration_date = visitor["vaccine"]["expiration_date"]
         if vaccine_expiration_date < datetime.date.today():
-            raise OutdatedVaccineError("Вакцина не дійсна")
+            raise OutdatedVaccineError("The vaccine has expired")
 
         if visitor["wearing_a_mask"] is False:
-            raise NotWearingMaskError("Вдягніть маску")
+            raise NotWearingMaskError("Put on a mask")
 
         return f"Welcome to {self.name}"
